ad906a94db36f43e74a25d95e13ea725639784bd
FlutLab.io <>
2022-09-18 12:50:30 +0000

    base2_web_updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,25 @@ import 'package:app/responsive/mobile_screen_layout.dart';
 import 'package:app/responsive/responsive_layout.dart';
 import 'package:app/responsive/web_screen_layout.dart';
 import 'package:app/utils/colors.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+      apiKey: "AIzaSyDsU0f0H1t6J6bxJKd8cu2kcU-lPely0P0",
+      appId: "1:386078821744:web:17bb5f8a3f7935d5dc93b5",
+      messagingSenderId: "386078821744",
+      projectId: "gangaputra-app",
+      storageBucket: "gangaputra-app.appspot.com",
+    ));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(MyApp());
 }
 
